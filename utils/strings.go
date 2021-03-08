bc5f9ed4cd63c578a1faca8ce032fcbb8757cab9
Mevan Samaratunga <mevansam@gmail.com>
2021-03-08 18:50:49 -0500

    added byte quantities to human readable format

--- utils/strings.go
+++ utils/strings.go
@@ -226,3 +226,40 @@ func RandomString(n int) string {
 	}
 	return string(b)
 }
+
+/*
+	Convert byte size to bytes, kilobytes, megabytes, gigabytes, ...
+
+	These utility functions convert a byte size to a human-readable string
+	in either SI (decimal) or IEC (binary) format.
+
+	Source: https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+*/
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func ByteCountIEC(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB",
+		float64(b)/float64(div), "KMGTPE"[exp])
+}
